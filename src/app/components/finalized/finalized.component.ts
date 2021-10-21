4ec0213995fb89e83e3951f679567407c3d6666c
Bruna Suelen <suelen.silva@ft.edu.br>
2021-10-21 15:19:06 -0300

    feat: Cria componente finalized

---
+++ src/app/components/finalized/finalized.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-finalized',
+  templateUrl: './finalized.component.html',
+  styleUrls: ['./finalized.component.css']
+})
+export class FinalizedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
